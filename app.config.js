1d83033dcf6b7dfecaec405b496e68b4a8bb1b43
fwagnersilva <fwagnersilva@gmail.com>
2025-10-06 11:51:37 -0300

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,41 @@
+export default {
+  expo: {
+    name: "GiroPro",
+    slug: "giropro",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+      bundler: "metro"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "backgroundColor": "#ffffff"
+        }
+      ]
+    ],
+    scheme: "giropro"
+  }
+}
